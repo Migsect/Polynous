1a9d22dc564ce37520fd84771ab7b5b3092d8800
Alex Perkins <perkwx@gmail.com>
2017-04-09 01:44:30 -0400

    Worked in prototype

--- prototype/cursor/scripts.js
+++ prototype/cursor/scripts.js
@@ -2,9 +2,56 @@
 
 var helloworld = "";
 
-const myfunction = function myfunction()
+var string = "abc";
+
+//1. Textarea
+//2. Javascript to change cursor color
+//3. When you type, use JS send current textarea content to server, update every second
+
+const stringposition = function stringposition(str)
 {
-  return 0;
+  //make the cursor move to the location of the click
+  return ":)";
+
 };
 
 console.log(helloworld, myfunction());
+
+const CURSOR_HTML = "<span class=\"cursor\"><div></div></span>";
+class TextLine
+{
+  constructor(text)
+  {
+    const self = this;
+    self.text = text;
+    self.cursorLocation = -1;
+  }
+
+  addCharacter(character)
+  {
+    const self = this;
+    // Add character at cursor location
+    self.text = text.substring(0, cursorLocation) + character + text.substring(cursorLocation);
+  }
+  getText()
+  {
+    const self = this;
+    // TODO add cursor
+    return self.text;
+  }
+  getCursorIndex()
+  {
+    const self = this;
+    return self.cursorLocation;
+  }
+  setCursorIndex(index)
+  {
+    const self = this;
+    self.cursorLocation = index;
+  }
+}
+
+const textArea = document.querySelector(".text-area");
+console.log(textArea);
+
+const textLine = new TextLine(textArea.innerHTML);
