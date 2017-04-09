eac8cff92d76c11935c15de449189c11b2eca88f
BLUEMAGIC\Aaron <acbluemagic@gmail.com>
2017-04-09 13:13:42 -0400

    my work

--- prototype/cursor/scripts.js
+++ prototype/cursor/scripts.js
@@ -1,5 +1,4 @@
 "use strict";
-var moo = prompt('who');
 var helloworld = "Hello World";
 
 const myfunction = function myfunction()
@@ -9,25 +8,32 @@ const myfunction = function myfunction()
 
 const textArea = document.querySelector("div.text-area");
 console.log(textArea);
-textArea.addEventListener("click", function(event){
-	textArea.innerHTML = "Aaron is a cool guy <3";
-});
-function insertBreakAtPoint(e) {
-
+textArea.addEventListener("click", function(e){
 var range;
 var textNode;
 var offset;
 
-if (document.caretPositionFromPoint) {    // standard
-    range = document.caretPositionFromPoint(e.pageX, e.pageY);
+if (document.caretPositionFromPoint) {
+    range = document.caretPositionFromPoint(e.clientX, e.clientY);
     textNode = range.offsetNode;
     offset = range.offset;
-
-} else if (document.caretRangeFromPoint) {    // WebKit
-    range = document.caretRangeFromPoint(e.pageX, e.pageY);
+  } else if (document.caretRangeFromPoint) {
+    range = document.caretRangeFromPoint(e.clientX, e.clientY);
     textNode = range.startContainer;
     offset = range.startOffset;
-}
+  }
 // do whatever you want here!
+ if (textNode.nodeType == 3) {
+    var replacement = textNode.splitText(offset);
+    var br = document.createElement('span');
+    br.classList.add("cursor");
+    var div = document.createElement("div");
+    br.appendChild(div);
+    textNode.parentNode.insertBefore(br, replacement);
+}
+	});
+
+  window.onload = function (){
+  document.getElementsByClassName("cursor").remove();
 }
 //console.log(helloworld, myfunction());
