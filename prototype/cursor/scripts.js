261e74b2f165e1b2688f534084208a7082e697f2
BLUEMAGIC\Aaron <acbluemagic@gmail.com>
2017-04-09 02:23:02 -0400

    my old stuff

--- prototype/cursor/scripts.js
+++ prototype/cursor/scripts.js
@@ -1,10 +1,33 @@
 "use strict";
-
-var helloworld = "";
+var moo = prompt('who');
+var helloworld = "Hello World";
 
 const myfunction = function myfunction()
 {
   return 0;
 };
 
-console.log(helloworld, myfunction());
+const textArea = document.querySelector("div.text-area");
+console.log(textArea);
+textArea.addEventListener("click", function(event){
+	textArea.innerHTML = "Aaron is a cool guy <3";
+});
+function insertBreakAtPoint(e) {
+
+var range;
+var textNode;
+var offset;
+
+if (document.caretPositionFromPoint) {    // standard
+    range = document.caretPositionFromPoint(e.pageX, e.pageY);
+    textNode = range.offsetNode;
+    offset = range.offset;
+
+} else if (document.caretRangeFromPoint) {    // WebKit
+    range = document.caretRangeFromPoint(e.pageX, e.pageY);
+    textNode = range.startContainer;
+    offset = range.startOffset;
+}
+// do whatever you want here!
+}
+//console.log(helloworld, myfunction());
